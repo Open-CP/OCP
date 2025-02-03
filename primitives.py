d6af993558277ce6f04deb42a7d2e2536129af9d
ChunningZhou <zhouchunning0510@gmail.com>
2025-02-03 11:03:41 +0800

    update primitives.py

--- primitives.py
+++ primitives.py
@@ -683,18 +683,58 @@ class Skinny_block_cipher(Block_cipher):
         
         p_bitsize, k_bitsize = version[0], version[1]
         if nbr_rounds==None: nbr_rounds=32 if (version[0],version[1])==(64,64) else 36 if (version[0],version[1])==(64,128) else 40 if (version[0],version[1])==(64,192)  else 40 if (version[0],version[1])==(128,128)  else 48 if (version[0],version[1])==(128,256)  else 56 if (version[0],version[1])==(128,384) else None
-        if model_type==0:  (s_nbr_layers, s_nbr_words, s_nbr_temp_words, s_word_bitsize), (k_nbr_layers, k_nbr_words, k_nbr_temp_words, k_word_bitsize), (sk_nbr_layers, sk_nbr_words, sk_nbr_temp_words, sk_word_bitsize) = (4, 16, 0, int(p_bitsize/16)), (4, int(16*k_bitsize / p_bitsize), 0, int(p_bitsize/16)), (4, 8, 0, int(p_bitsize/16))
+        tweak_size = int(k_bitsize/p_bitsize)
+        if model_type==0: 
+            if tweak_size ==1: (s_nbr_layers, s_nbr_words, s_nbr_temp_words, s_word_bitsize), (k_nbr_layers, k_nbr_words, k_nbr_temp_words, k_word_bitsize), (sk_nbr_layers, sk_nbr_words, sk_nbr_temp_words, sk_word_bitsize) = (5, 16, 0, int(p_bitsize/16)), (1, int(16*k_bitsize / p_bitsize), 0, int(p_bitsize/16)), (1, 8, 0, int(p_bitsize/16))
+            elif tweak_size == 2 or tweak_size ==3: (s_nbr_layers, s_nbr_words, s_nbr_temp_words, s_word_bitsize), (k_nbr_layers, k_nbr_words, k_nbr_temp_words, k_word_bitsize), (sk_nbr_layers, sk_nbr_words, sk_nbr_temp_words, sk_word_bitsize) = (5, 16, 0, int(p_bitsize/16)), (5, int(16*k_bitsize / p_bitsize), 8, int(p_bitsize/16)), (1, 8, 0, int(p_bitsize/16))
+        
         super().__init__(name, p_input, k_input, c_output, nbr_rounds, [s_nbr_layers, s_nbr_words, s_nbr_temp_words, s_word_bitsize], [k_nbr_layers, k_nbr_words, k_nbr_temp_words, k_word_bitsize], [sk_nbr_layers, sk_nbr_words, sk_nbr_temp_words, sk_word_bitsize])
-                
+        round_constants = [0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3E, 0x3D, 0x3B, 0x37, 0x2F, 0x1E, 0x3C, 0x39, 0x33,
+                                  0x27, 0x0E, 0x1D, 0x3A, 0x35, 0x2B, 0x16, 0x2C, 0x18, 0x30, 0x21, 0x02, 0x05, 0x0B,
+                                  0x17, 0x2E, 0x1C, 0x38, 0x31, 0x23, 0x06, 0x0D, 0x1B, 0x36, 0x2D, 0x1A, 0x34, 0x29,
+                                  0x12, 0x24, 0x08, 0x11, 0x22, 0x04, 0x09, 0x13, 0x26, 0x0c, 0x19, 0x32, 0x25, 0x0a,
+                                  0x15, 0x2a, 0x14, 0x28, 0x10, 0x20]
+        k_perm = [9,15,8,13,10,14,12,11,0,1,2,3,4,5,6,7]
+        
         # create constraints
         if model_type==0:
-            for i in range(1,nbr_rounds+1):              
-                self.states["STATE"].SboxLayer("SB", i, 0, op.Skinny_4bit_Sbox)  # Sbox layer            
-                self.states["STATE"].AddConstantLayer("C", i, 1, "xor", [0,0,0,0, 0,0,0,0, 2,0,0,0, 0,0,0,0])  # Constant layer      
-                self.states["STATE"].PermutationLayer("SR", i, 2, [0,1,2,3, 7,4,5,6, 10,11,8,9, 13,14,15,12]) # Shiftrows layer
-                self.states["STATE"].MatrixLayer("MC", i, 3, [[1,0,1,1], [1,0,0,0], [0,1,1,0], [1,0,1,0]], [[0,4,8,12], [1,5,9,13], [2,6,10,14], [3,7,11,15]])  #Mixcolumns layer
+
+            # subkeys extraction
+            for i in range(1,nbr_rounds+1): 
+                if tweak_size == 1:
+                    self.states["SUBKEYS"].ExtractionLayer("SK_EX", i, 0, [i for i in range(8)], self.states["KEY_STATE"].vars[i][0])
+                elif tweak_size >= 2:
+                    self.states["SUBKEYS"].ExtractionLayer("SK_EX", i+1, 0, [i for i in range(tweak_size*16,tweak_size*16+8)], self.states["KEY_STATE"].vars[i][0])
             
-            pass # TODO key schedule and subkey
+
+            # key schedule
+            k_perm_T = [i + 16 * j for j in range(int(k_bitsize/p_bitsize)) for i in k_perm]
+            if tweak_size == 1:
+                for i in range(1, nbr_rounds+1): 
+                    self.states["KEY_STATE"].PermutationLayer("K_P", i, 0, k_perm_T) # Permutation layer
+            # elif tweak_size == 2: # TO DO 
+            #     self.states["KEY_STATE"].SingleOperatorLayer("K_XOR", 1, 0, op.bitwiseXOR, [[j,16+j] for j in range(8)], [j for j in range(32,40)]) # XOR layer 
+            #     for i in range(2, nbr_rounds+2): 
+            #         self.states["KEY_STATE"].PermutationLayer("K_P", i, 0, k_perm_T) # Permutation layer
+                     
+            # elif tweak_size == 3: # TO DO
+            #     self.states["KEY_STATE"].SingleOperatorLayer("K_XOR", 1, 0, op.bitwiseXOR, [[j,16+j] for j in range(8)], [j for j in range(48,56)]) # XOR layer 
+            #     self.states["KEY_STATE"].SingleOperatorLayer("K_XOR", 1, 1, op.bitwiseXOR, [[32+j,48+j] for j in range(8)], [j for j in range(48,56)]) # XOR layer   
+            #     for i in range(2, nbr_rounds+2): 
+            #         self.states["KEY_STATE"].PermutationLayer("K_P", i, 0, k_perm_T) # Permutation layer
+                 
+            # Internal permutation
+            for i in range(1,nbr_rounds+1):  
+                if s_word_bitsize==4: self.states["STATE"].SboxLayer("SB", i, 0, op.Skinny_4bit_Sbox) 
+                else: self.states["STATE"].SboxLayer("SB", i, 0, op.Skinny_8bit_Sbox)  # Sbox layer                        
+                rc = round_constants[i-1]
+                c0 = rc & 0xF
+                c1 = rc >> 4
+                c2 = 0x2     
+                self.states["STATE"].AddConstantLayer("C", i, 1, "xor", [c0,None,None,None, c1,None,None,None, c2,None,None,None, None,None,None,None])  # Constant layer            
+                self.states["STATE"].AddRoundKeyLayer("ARK", i, 2, op.bitwiseXOR, self.states["SUBKEYS"], mask=[1 for i in range(8)])  # AddRoundKey layer   
+                self.states["STATE"].PermutationLayer("SR", i, 3, [0,1,2,3, 7,4,5,6, 10,11,8,9, 13,14,15,12]) # Shiftrows layer
+                self.states["STATE"].MatrixLayer("MC", i, 4, [[1,0,1,1], [1,0,0,0], [0,1,1,0], [1,0,1,0]], [[0,4,8,12], [1,5,9,13], [2,6,10,14], [3,7,11,15]])  #Mixcolumns layer
                 
 
 # The AES block cipher
