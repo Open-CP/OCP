5582e2f88259a66b2b0febce2b11b1d465dc362d
ChunningZhou <zhouchunning0510@gmail.com>
2025-03-12 21:52:51 +0800

    add gift.py

---
+++ primitives/gift.py
@@ -0,0 +1,84 @@
+from primitives.primitives import Permutation, Block_cipher
+import operators.operators as op
+
+
+# The GIFT internal permutation               
+class GIFT_permutation(Permutation):
+    def __init__(self, name, version, s_input, s_output, nbr_rounds=None, represent_mode=0):
+        """
+        Initialize the GIFT internal permutation
+        :param name: Name of the permutation
+        :param version: Bit size of the permutation
+        :param s_input: Input state
+        :param s_output: Output state
+        :param nbr_rounds: Number of rounds
+        :param represent_mode: Integer specifying the mode of representation used for encoding the cipher.
+        """
+        
+        if nbr_rounds==None: nbr_rounds=28 if version==64 else 40 if version==128 else None
+        if represent_mode==0: nbr_layers, nbr_words, nbr_temp_words, word_bitsize = 3, version, 0, 1
+        super().__init__(name, s_input, s_output, nbr_rounds, [nbr_layers, nbr_words, nbr_temp_words, word_bitsize])
+        
+        constant_table = self.gen_rounds_constant_table()
+        if version == 64: perm = [12, 1, 6, 11, 28, 17, 22, 27, 44, 33, 38, 43, 60, 49, 54, 59, 8, 13, 2, 7, 24, 29, 18, 23, 40, 45, 34, 39, 56, 61, 50, 55, 4, 9, 14, 3, 20, 25, 30, 19, 36, 41, 46, 35, 52, 57, 62, 51, 0, 5, 10, 15, 16, 21, 26, 31, 32, 37, 42, 47, 48, 53, 58, 63]
+        elif version == 128: perm = [12, 1, 6, 11, 28, 17, 22, 27, 44, 33, 38, 43, 60, 49, 54, 59, 76, 65, 70, 75, 92, 81, 86, 91, 108, 97, 102, 107, 124, 113, 118, 123, 8, 13, 2, 7, 24, 29, 18, 23, 40, 45, 34, 39, 56, 61, 50, 55, 72, 77, 66, 71, 88, 93, 82, 87, 104, 109, 98, 103, 120, 125, 114, 119, 4, 9, 14, 3, 20, 25, 30, 19, 36, 41, 46, 35, 52, 57, 62, 51, 68, 73, 78, 67, 84, 89, 94, 83, 100, 105, 110, 99, 116, 121, 126, 115, 0, 5, 10, 15, 16, 21, 26, 31, 32, 37, 42, 47, 48, 53, 58, 63, 64, 69, 74, 79, 80, 85, 90, 95, 96, 101, 106, 111, 112, 117, 122, 127]
+        
+        # create constraints
+        if represent_mode==0:
+            for i in range(1,nbr_rounds+1):              
+                self.states["STATE"].SboxLayer("SB", i, 0, op.GIFT_Sbox,index=[list(range(i, i + 4)) for i in range(0, nbr_words, 4)])  # Sbox layer            
+                self.states["STATE"].PermutationLayer("P", i, 1, perm) # Permutation layer
+                self.states["STATE"].AddConstantLayer("C", i, 2, "xor", [True]+[None]*(version-25)+[True]+[None, None, None, True]*5, constant_table)# Constant layer                      
+                
+    def gen_rounds_constant_table(self):
+        constant_table = []
+        const = [0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3E, 0x3D, 0x3B, 0x37, 0x2F, 0x1E, 0x3C, 0x39, 0x33, 0x27, 0x0E, 0x1D, 0x3A, 0x35, 0x2B, 0x16, 0x2C, 0x18, 0x30, 0x21, 0x02, 0x05, 0x0B, 0x17, 0x2E, 0x1C, 0x38, 0x31, 0x23, 0x06, 0x0D, 0x1B, 0x36, 0x2D, 0x1A, 0x34, 0x29, 0x12, 0x24, 0x08, 0x11, 0x22, 0x04, 0x09, 0x13, 0x26, 0x0c, 0x19, 0x32, 0x25, 0x0a, 0x15, 0x2a, 0x14, 0x28, 0x10, 0x20]
+        for i in range(1,self.states["STATE"].nbr_rounds+1):  
+            constant_table.append([1, (const[i-1]>>5)&1, (const[i-1]>>4)&1, (const[i-1]>>3)&1, (const[i-1]>>2)&1, (const[i-1]>>1)&1, (const[i-1])&1])
+        return constant_table
+
+
+# The GIFT block cipher              
+class GIFT_block_cipher(Block_cipher):
+    def __init__(self, name, version, p_input, k_input, c_output, nbr_rounds=None, represent_mode=0):
+        """
+        Initializes the GIFT block cipher.
+        :param name: Cipher name
+        :param version: (p_bitsize, k_bitsize)
+        :param p_input: Plaintext input
+        :param k_input: Key input
+        :param c_output: Ciphertext output
+        :param nbr_rounds: Number of rounds
+        :param represent_mode: Integer specifying the mode of representation used for encoding the cipher.
+        """
+        p_bitsize, k_bitsize = version[0], version[1]
+        if nbr_rounds==None: nbr_rounds=28 if p_bitsize==64 else 40 if p_bitsize==128 else None
+        if represent_mode==0: (s_nbr_layers, s_nbr_words, s_nbr_temp_words, s_word_bitsize), (k_nbr_layers, k_nbr_words, k_nbr_temp_words, k_word_bitsize), (sk_nbr_layers, sk_nbr_words, sk_nbr_temp_words, sk_word_bitsize) = (4, p_bitsize, 0, 1),  (1, k_bitsize, 0, 1),  (1, int(p_bitsize/2), 0, 1)
+        super().__init__(name, p_input, k_input, c_output, nbr_rounds, nbr_rounds, [s_nbr_layers, s_nbr_words, s_nbr_temp_words, s_word_bitsize], [k_nbr_layers, k_nbr_words, k_nbr_temp_words, k_word_bitsize], [sk_nbr_layers, sk_nbr_words, sk_nbr_temp_words, sk_word_bitsize])
+        
+        constant_table = self.gen_rounds_constant_table()
+        if p_bitsize == 64: perm = [12, 1, 6, 11, 28, 17, 22, 27, 44, 33, 38, 43, 60, 49, 54, 59, 8, 13, 2, 7, 24, 29, 18, 23, 40, 45, 34, 39, 56, 61, 50, 55, 4, 9, 14, 3, 20, 25, 30, 19, 36, 41, 46, 35, 52, 57, 62, 51, 0, 5, 10, 15, 16, 21, 26, 31, 32, 37, 42, 47, 48, 53, 58, 63]
+        elif p_bitsize == 128: perm = [12, 1, 6, 11, 28, 17, 22, 27, 44, 33, 38, 43, 60, 49, 54, 59, 76, 65, 70, 75, 92, 81, 86, 91, 108, 97, 102, 107, 124, 113, 118, 123, 8, 13, 2, 7, 24, 29, 18, 23, 40, 45, 34, 39, 56, 61, 50, 55, 72, 77, 66, 71, 88, 93, 82, 87, 104, 109, 98, 103, 120, 125, 114, 119, 4, 9, 14, 3, 20, 25, 30, 19, 36, 41, 46, 35, 52, 57, 62, 51, 68, 73, 78, 67, 84, 89, 94, 83, 100, 105, 110, 99, 116, 121, 126, 115, 0, 5, 10, 15, 16, 21, 26, 31, 32, 37, 42, 47, 48, 53, 58, 63, 64, 69, 74, 79, 80, 85, 90, 95, 96, 101, 106, 111, 112, 117, 122, 127]
+        
+        # create constraints
+        if represent_mode==0:
+            for i in range(1,nbr_rounds+1):    
+                # subkeys extraction
+                self.states["SUBKEYS"].ExtractionLayer("SK_EX", i, 0, [item for pair in zip(range(160-p_bitsize, 160-3*int(p_bitsize/4)), range(128-int(p_bitsize/4), 128)) for item in pair], self.states["KEY_STATE"].vars[i][0])
+                
+                # key schedule
+                self.states["KEY_STATE"].PermutationLayer("PERM", i, 0, [110,111]+[i for i in range(96,110)]+[i for i in range(116,128)]+[i for i in range(112,116)]+[i for i in range(96)]) # key schedule 
+        
+                # Internal permutation          
+                self.states["STATE"].SboxLayer("SB", i, 0, op.GIFT_Sbox,index=[list(range(i, i + 4)) for i in range(0, s_nbr_words, 4)])  # Sbox layer            
+                self.states["STATE"].PermutationLayer("P", i, 1, perm) # Permutation layer
+                self.states["STATE"].AddConstantLayer("C", i, 2, "xor", [True]+[None]*(p_bitsize-25)+[True]+[None, None, None, True]*5, constant_table)# Constant layer                      
+                if p_bitsize == 64: self.states["STATE"].AddRoundKeyLayer("ARK", i, 3, op.bitwiseXOR, self.states["SUBKEYS"], [0,0,1,1]*16) # Addroundkey layer 
+                elif p_bitsize == 128: self.states["STATE"].AddRoundKeyLayer("ARK", i, 3, op.bitwiseXOR, self.states["SUBKEYS"], [0,1,1,0]*32) # Addroundkey layer 
+
+    def gen_rounds_constant_table(self):
+        constant_table = []
+        const = [0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3E, 0x3D, 0x3B, 0x37, 0x2F, 0x1E, 0x3C, 0x39, 0x33, 0x27, 0x0E, 0x1D, 0x3A, 0x35, 0x2B, 0x16, 0x2C, 0x18, 0x30, 0x21, 0x02, 0x05, 0x0B, 0x17, 0x2E, 0x1C, 0x38, 0x31, 0x23, 0x06, 0x0D, 0x1B, 0x36, 0x2D, 0x1A, 0x34, 0x29, 0x12, 0x24, 0x08, 0x11, 0x22, 0x04, 0x09, 0x13, 0x26, 0x0c, 0x19, 0x32, 0x25, 0x0a, 0x15, 0x2a, 0x14, 0x28, 0x10, 0x20]
+        for i in range(1,self.states["STATE"].nbr_rounds+1):  
+            constant_table.append([1, (const[i-1]>>5)&1, (const[i-1]>>4)&1, (const[i-1]>>3)&1, (const[i-1]>>2)&1, (const[i-1]>>1)&1, (const[i-1])&1])
+        return constant_table
