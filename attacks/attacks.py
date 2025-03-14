ddb5f3d1760533ba133ca4662014a96b9892a623
ChunningZhou <zhouchunning0510@gmail.com>
2025-03-14 14:50:23 +0100

    update attacks.py

--- attacks/attacks.py
+++ attacks/attacks.py
@@ -229,8 +229,6 @@ def diff_attacks(cipher, add_constraints=[], model_type="milp", goal="search_opt
         while not sol:
             model, variable_map = attacks(cipher, add_constraints=add_constraints, model_type=model_type, obj_sat=obj_sat, filename=filename)
             sol = solving.solve_sat(filename, variable_map)
-            print(variable_map)
-            print(sol)
             obj_sat += 1
         solving.formulate_solutions(cipher, sol)
         obj = obj_sat-1 
