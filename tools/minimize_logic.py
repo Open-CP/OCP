a10c8aa2aaf65f102b8cb9f961762a2223068b9f
ChunningZhou <zhouchunning0510@gmail.com>
2025-04-05 16:54:17 +0800

    update minimize_logic.py

--- tools/minimize_logic.py
+++ tools/minimize_logic.py
@@ -3,6 +3,7 @@ try:
 except ImportError:
     print("pyeda is not installed, installing it by 'pip3 install pyeda', https://pyeda.readthedocs.io/en/latest/")        
 import subprocess
+import os
 
 
 def ttb_to_cnf_espresso(model_type, ttable, num_vars, variables, mode=0):    
@@ -16,17 +17,22 @@ def ttb_to_cnf_espresso(model_type, ttable, num_vars, variables, mode=0):
     file_contents += ".ob F\n"
     file_contents += ".type fr\n" 
     file_contents += cont_ttable
-    with open('files/ttable.txt', "w") as fw:
+    script_dir = os.path.dirname(os.path.abspath(__file__)) 
+    base_path = os.path.join(script_dir, '..', 'files')
+    base_path = os.path.abspath(base_path)
+    filename_ttable = os.path.join(base_path, 'ttable.txt')
+    filename_sttable = os.path.join(base_path, 'sttable.txt')
+    with open(filename_ttable, "w") as fw:
         fw.write(file_contents)
         fw.write(".e\n")
 
     espresso_options =  [['-estrong', '-eonset'], [], ['-eonset']]
-    espresso_command = ['espresso', *espresso_options[mode], 'files/ttable.txt'] # Espresso Script of Pyeda provides the parameters: "-e {fast,ness,nirr,nunwrap,onset,strong}"
+    espresso_command = ['espresso', *espresso_options[mode], filename_ttable] # Espresso Script of Pyeda provides the parameters: "-e {fast,ness,nirr,nunwrap,onset,strong}"
     result = subprocess.run(espresso_command, capture_output=True, text=True)
     espresso_output = result.stdout
-    with open('files/sttable.txt', 'w') as fw: 
+    with open(filename_sttable, 'w') as fw: 
         fw.write(espresso_output)
-    with open('files/sttable.txt', 'r') as fr: 
+    with open(filename_sttable, 'r') as fr: 
         espresso_output = fr.readlines()   
 
     constraints = []
